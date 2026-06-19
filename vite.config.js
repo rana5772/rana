d8ff943c5505bb6f59bae5ed1cc834d6fe85d5d1
rana5772 <vaibhav.rana5772@gmail.com>
2026-06-20 02:23:23 +0530

    removed visualiser

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { fileURLToPath, URL } from "node:url";
-import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    visualizer({
-      open: true,
-      gzipSize: true,
-      brotliSize: true,
-      filename: "stats.html",
-    }),
   ],
   server: { host: true, port: 3000 },
   resolve: {
