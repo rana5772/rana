a723190d97819f0bd37b120a40ac436bfc586ad9
rana5772 <vaibhav.rana5772@gmail.com>
2026-06-23 00:09:52 +0530

    analtics and speed

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,24 @@
 // main.jsx
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
+
 import "./styles/tokens.css";
 import "./index.css";
 import "./style.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
-import { HelmetProvider } from "react-helmet-async";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* Move HelmetProvider OUTSIDE BrowserRouter */}
     <HelmetProvider>
       <BrowserRouter>
         <App />
+        <Analytics />
+        <SpeedInsights />
       </BrowserRouter>
     </HelmetProvider>
-  </StrictMode>,
+  </StrictMode>
 );
